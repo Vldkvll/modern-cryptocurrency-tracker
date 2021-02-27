8229effe6d84c68a985e6465855c10faa3c474fb
Vladyslav Koval <49593840+Vldkvll@users.noreply.github.com>
2021-02-27 16:43:14 +0200

    added api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { getCoinApi } from "./api/api";
 
 function App() {
-  return (
-    <div >
-     
-    </div>
-  );
+    const [coin, setCoin] = React.useState({});
+
+    React.useEffect(() => {
+        async function fetchCoin() {
+            const response = await getCoinApi();
+            console.log(response);
+            setCoin((prev) => (prev = response));
+        }
+
+        fetchCoin();
+    }, []);
+
+    console.log(coin);
+
+    return (
+        <div>
+            <ul>
+                {coin.map((item) => (
+                    <li>{item.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    headers: {
+        contentType: 'application/json'
+    },
+    baseURL: `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`,
+    
+})
+
+export const getCoinApi = () => {
+    return instance.get()
+        .then(function (response) {
+            return response.data
+        })
+}
